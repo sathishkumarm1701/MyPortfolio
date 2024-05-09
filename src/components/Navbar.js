2caf475c4b9b8acdc74545bb648d06436c53b9db
Sathish m <94674492+sathishkumarm1701@users.noreply.github.com>
2024-05-09 22:18:18 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -103,7 +103,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/soumyajit4419/Portfolio"
+                href="https://github.com/sathishkumarm1701/MyPortfolio"
                 target="_blank"
                 className="fork-btn-inner"
               >
